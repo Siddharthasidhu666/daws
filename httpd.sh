fcd124048a815e9af76243c9a70a9b7499cc66c9
siddhu <siddhumahesh333@gmail.com>
2024-07-21 00:04:03 +0530

    Hst

--- httpd.sh
+++ httpd.sh
@@ -7,10 +7,19 @@ else
    echo "this is root user"
 fi
 
-yum install httpd -y
+yum install http -y
 
 if [ $? -ne 0 ]; then
     echo "failed to install httpd"
+    exit 1
+else 
+    echo " install success"
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]; then
+    echo "failed to install git"
     exit 1
 else 
     echo " install success"
