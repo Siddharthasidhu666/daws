f6e47e568a963c218cdde2615dc7d93ace356ede
siddhu <siddhumahesh333@gmail.com>
2024-07-20 15:00:21 +0530

    data

--- datatypes.sh
+++ datatypes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-num1 = $1
-num2 = $2
-sum = ( $num1 + $num2 )
+num1=$1
+num2=$2
+sum = ($num1 + $num2)
 echo " sum is :: $sum "
 echo "  how many args passed is :: $# "
 echo " script name is :: $0 "
