b396e9c798f27a34c8d3a865bf04e819dd08975f
siddhu <siddhumahesh333@gmail.com>
2024-07-20 23:59:35 +0530

    HT

--- httpd.sh
+++ httpd.sh
@@ -6,9 +6,10 @@ if [ $output -ne 0 ]; then
 else
    echo "this is root user"
 fi
+
 yum install http -y
 
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "failed to install httpd"
     exit 1
 else 
