148216ab6384d465202444efc0a33c1de266a658
siddhu <siddhumahesh333@gmail.com>
2024-07-20 23:57:22 +0530

    HTT

--- httpd.sh
+++ httpd.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 output=$(id -u)
-if [ $output -gt 0 ]; then
+if [ $output -ne 0 ]; then
    echo "this is not root user"
    exit 1
 else
    echo "this is root user"
+fi
+yum install http -y
+
+if [$? -ne 0]; then
+    echo "failed to install httpd"
+    exit 1
+else 
+    echo "success"
 fi
