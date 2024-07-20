0e06fb7624c2ffa9224f4095a5666e10f4dd9853
siddhu <siddhumahesh333@gmail.com>
2024-07-20 14:45:06 +0530

    hevsa

---
+++ 3-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1 : hi $person2"
+echo "$person2 : hello $person1"
+echo "$person1 : what are you doing $person2"
+echo "$person2 : Nothing $person1"
